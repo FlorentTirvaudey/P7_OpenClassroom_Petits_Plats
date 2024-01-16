c2470df73edb68a7765588ed86f98bc158da40ca
FlorentTirvaudey <florent.tirvaudey@gmail.com>
2024-01-16 11:40:13 +0100

    clean code et quasi finalisation première version algo

--- recipesection.js
+++ recipesection.js
@@ -10,8 +10,11 @@ const hiddenIngredientsList = document.getElementById("hidden_ingredients_in_dro
 const hiddenAppareilsList = document.getElementById("hidden_appareils_in_dropdown");
 const hiddenUstensilsList = document.getElementById("hidden_ustensils_in_dropdown");
 
-const noeudParent = document.getElementById("search_container_created");
+const ingreInput = document.getElementById("ingredient_input");
+const apparInput = document.getElementById("appareils_input");
+const ustenInput = document.getElementById("ustensiles_input");
 
+const tagsSection = document.getElementById("search_container_created");
 
 function getAllIngredients(data) {
 	let ingredientTab = [];
@@ -81,12 +84,6 @@ function openDropdownMenu() {
 	openDropdownMenuCSS(ustensilesBtn, searchbarMenuUstensiles, chevronBtnUst);
 }
 
-// function removeOnclickCross(cross, noeud) {
-// 	cross.addEventListener("click", () => {
-// 		noeud.remove();
-// 	})
-// }
-
 function createSearchContainer(element, noeudToRemove, datasFromRecipes) {
 
 	const searchContainer = document.createElement( "div" );
@@ -108,84 +105,56 @@ function createSearchContainer(element, noeudToRemove, datasFromRecipes) {
 	searchContainer.appendChild(elementContent);
 	searchContainer.appendChild(crossButton);
 
-	noeudParent.appendChild(searchContainer);
+	tagsSection.appendChild(searchContainer);
 
-	crossButton.addEventListener("click", e => {
-		// const otherCrossToRemove = document.getElementById("search_item_in_dropdown");
+	crossButton.addEventListener("click", () => {
 		const itemToRemove = Array.from(noeudToRemove.children).filter(element => element.textContent === elementContent.textContent)[0];
+
+		if (itemToRemove) {
+			itemToRemove.remove();
+		}
 		searchContainer.remove();
-		itemToRemove.remove();
-		// console.log(e.target.parentElement.previousElementSibling.textContent)
 
-		// const otherCrossToRemove = document.getElementById("hidden_ustensils_in_dropdown")
 		filteredWithTags(recipes);
-		// const itemsInNoeudParent = Array.from(noeudParent.children);
-		// console.log("itemsInNoeudParent avant vjdgjkgzkj", itemsInNoeudParent)	
-
-		// console.log("e", e.target.parentElement.previousElementSibling.textContent)
-		// // itemsInNoeudParent.map(element => element.textContent.toLowerCase() === e.target.parentElement.previousElementSibling.textContent.toLowerCase()).remove();
-		// console.log("itemsInNoeudParent", itemsInNoeudParent)	
-
-
-		// // console.log(itemToRemove);
-		// // console.log(searchContainer);
-
-		// if (!itemsInNoeudParent.length) {
-			
-
-		// 	const updatedIngredients = getAllIngredients(recipes);
-		// 	const updatedAppareils = getAllAppareils(recipes);
-		// 	const updatedUstensils = getAllUstensils(recipes);
-
-		// 	updateRecipeCard(recipes, nbRecipes);
-
-		// 	updateDataInDropdownMenu(ingreInDropdown, updatedIngredients, hiddenIngredientsList, recipes);
-		// 	updateDataInDropdownMenu(apparInDropdown, updatedAppareils, hiddenAppareilsList, recipes);
-		// 	updateDataInDropdownMenu(ustenInDropdown, updatedUstensils, hiddenUstensilsList, recipes);
-
-		// } else {
-		
-
-		// // const filteredDatas = Array.from(noeudParent.children).forEach(element => datasFromRecipes.filter(datas => datas.ingredients.find(data => data.ingredient.toLowerCase() === element)
-		// // || datas.appliance.trim().toLowerCase().includes(element) 
-		// // || datas.ustensils.find(data => data.toLowerCase() === element)));
-
-		// const filteredDatas = recipes.filter(datas =>
-		// 	itemsInNoeudParent.find(element =>
-		// 		datas.appliance.trim().toLowerCase().includes(element.textContent.toLowerCase())
-		// 		|| datas.ingredients.some(data => data.ingredient.toLowerCase() === element.textContent.toLowerCase())
-		// 		|| datas.ustensils.some(data => data.toLowerCase() === element.textContent.toLowerCase())
-		// 	)
-		// );
-
-		// const updatedIngredients = getAllIngredients(filteredDatas);
-		// const updatedAppareils = getAllAppareils(filteredDatas);
-		// const updatedUstensils = getAllUstensils(filteredDatas);
-
-		// updateRecipeCard(filteredDatas, nbRecipes);
-
-		// updateDataInDropdownMenu(ingreInDropdown, updatedIngredients, hiddenIngredientsList, filteredDatas);
-		// updateDataInDropdownMenu(apparInDropdown, updatedAppareils, hiddenAppareilsList, filteredDatas);
-		// updateDataInDropdownMenu(ustenInDropdown, updatedUstensils, hiddenUstensilsList, filteredDatas);
-
-		// console.log("filteredDatas in addbfhbjlkefj", filteredDatas)
-
-		// console.log("itemsInNoeudParent last remove", itemsInNoeudParent)	
-		
-		// }
-
 	})
 }
 
-function createContainerInDropdown(element, noeudParent, datasFromRecipes) {
+function createSearchContainerFromMainInput(element, datasFromRecipes) {
+
+	const searchContainer = document.createElement( "div" );
+	searchContainer.setAttribute("id", "search_item_in_new_container");
+	searchContainer.setAttribute("class", "flex flex-row justify-between items-center p-6 bg-yellow-400 rounded-[15px] w-[20%]");
+
+	const elementContent = document.createElement( "span" );
+	elementContent.setAttribute("class", "flex align-middle text-2xl");
+	elementContent.textContent = element;
+
+	const crossButton = document.createElement( "button" );
+	crossButton.setAttribute("class", "text-2xl");
+
+	const crossIcon = document.createElement( "i" );
+	crossIcon.setAttribute("class", "fa-solid fa-xmark");
+
+	crossButton.appendChild(crossIcon);
+
+	searchContainer.appendChild(elementContent);
+	searchContainer.appendChild(crossButton);
+
+	tagsSection.appendChild(searchContainer);
+
+	crossButton.addEventListener("click", () => {
+
+		searchContainer.remove();
+
+		filteredWithTags(recipes);
+	})
+}
+
+function createContainerInDropdown(element, tagsSection, datasFromRecipes) {
 	const selectHiddenItems = document.createElement( "li" );
-	selectHiddenItems.setAttribute("class", "flex text-xl justify-between align-middle px-2 py-2");
+	selectHiddenItems.setAttribute("class", "flex text-xl justify-between align-middle px-2 py-2 group-hover:block");
 	selectHiddenItems.setAttribute("id", "search_item_in_dropdown");
 	selectHiddenItems.textContent = element;
-
-	// const elementInHiddenList = document.createElement( "span" );
-	// elementInHiddenList.setAttribute("class", "font-['Manrope']");
-	// elementInHiddenList.textContent = element;
 
 	const crossButtonInDropdown = document.createElement( "button" );
 	crossButtonInDropdown.setAttribute("class", "text-xl hidden");
@@ -197,118 +166,55 @@ function createContainerInDropdown(element, noeudParent, datasFromRecipes) {
 
 	selectHiddenItems.appendChild(crossButtonInDropdown);
 
-	noeudParent.appendChild(selectHiddenItems);
+	tagsSection.appendChild(selectHiddenItems);
 
-	// selectHiddenItems.addEventListener("mouseover", () => {
-	// 	crossButtonInDropdown.classList.remove("hidden");
-	// 	console.log("test")
-	// })
+	selectHiddenItems.addEventListener("mouseover", () => {
+		crossButtonInDropdown.classList.remove("hidden");
+	})
 
-	crossButtonInDropdown.addEventListener("click", e => {
+	selectHiddenItems.addEventListener("mouseout", () => {
+		crossButtonInDropdown.classList.add("hidden");
+	})
+
+	crossButtonInDropdown.addEventListener("click", () => {
 		const otherCrossToRemove = document.getElementById("search_item_in_new_container");
-		console.log(e)
-
-		// const itemToRemove = Array.from(noeudToRemove.children).filter(element => element.textContent === elementContent.textContent)[0];
 
 		selectHiddenItems.remove();
 		otherCrossToRemove.remove();
+
+		filteredWithTags(recipes);
 	})
 }
 
 function filteredWithTags(datasFromRecipes) {
 
-	// const ingreInput = document.getElementById("ingredient_input");
-	// const apparInput = document.getElementById("appareils_input");
-	// const ustenInput = document.getElementById("ustensiles_input");
-
-	// // const ingredients = getAllIngredients(recipes);
-	// // const appareils = getAllAppareils(recipes);
-	// // const ustensils = getAllUstensils(recipes);
-
-	// const eventValue = e.target.textContent.trim().toLowerCase();
-	// // console.log("dataRecipes.ustensils ici", datasFromRecipes[0].ustensils);
-
-	// const filteredDatas = datasFromRecipes.filter(element => element.ingredients.find(data => data.ingredient.toLowerCase() === eventValue)
-	// || element.appliance.trim().toLowerCase().includes(eventValue) 
-	// || element.ustensils.find(data =>
-	// 	// console.log("data en comparaison", data.trim().toLowerCase())
-	// 	// console.log("eventValue en comparaison", eventValue)
-	// 	data.toLowerCase() === eventValue
-	// ));
-	// // console.log("test un deux", datasFromRecipes.filter(element => element.ustensils.find(data => data.toLowerCase() === eventValue)))
-
-	// // const vardetest = datasFromRecipes.forEach(element => {
-	// // 	element.ustensils.forEach(data => {
-	// // 		if(!data.trim().toLowerCase().includes(eventValue)) {
-	// // 			console.log("ne correspond pas", data.trim().toLowerCase())
-	// // 		}
-	// // 	})
-	// // })
-
-	// // const filteredDatas = datasFromRecipes.filter(element => element.name.toLowerCase().includes(eventValue) 
-	// // 	|| element.description.toLowerCase().includes(eventValue) 
-	// // 	|| element.ingredients.forEach(data => {
-	// // 		data.ingredient.toLowerCase().includes(eventValue);
-	// // 		if(data.ingredient.toLowerCase().includes(eventValue)) {
-	// // 			console.log("true include");
-	// // 		}
-	// // 	})
-	// // 	);
-		
-	// console.log("les datas filtrer avec tags ghgfg", filteredDatas);
-	// console.log("les datas dans dataRecipe", datasFromRecipes)
-
-	// const updatedIngredients = getAllIngredients(filteredDatas);
-	// const updatedAppareils = getAllAppareils(filteredDatas);
-	// const updatedUstensils = getAllUstensils(filteredDatas);
-
-	// updateRecipeCard(filteredDatas, nbRecipes);
-
-	// updateDataInDropdownMenu(ingreInDropdown, updatedIngredients, hiddenIngredientsList, filteredDatas);
-	// updateDataInDropdownMenu(apparInDropdown, updatedAppareils, hiddenAppareilsList, filteredDatas);
-	// updateDataInDropdownMenu(ustenInDropdown, updatedUstensils, hiddenUstensilsList, filteredDatas);
-
-	const itemsInNoeudParent = Array.from(noeudParent.children);
-		console.log("itemsInNoeudParent avant vjdgjkgzkj", itemsInNoeudParent)	
-
-		// console.log("e", e.target.parentElement.previousElementSibling.textContent)
-		// itemsInNoeudParent.map(element => element.textContent.toLowerCase() === e.target.parentElement.previousElementSibling.textContent.toLowerCase()).remove();
-		console.log("itemsInNoeudParent", itemsInNoeudParent)	
-
-		console.log("tell me the truth", datasFromRecipes)	
-
-		// console.log(itemToRemove);
-		// console.log(searchContainer);
-
-		if (!itemsInNoeudParent.length) {
+	const itemsInTagsSection = Array.from(tagsSection.children);
+		if (!itemsInTagsSection.length) {
 			
-
-			const updatedIngredients = getAllIngredients(recipes);
-			const updatedAppareils = getAllAppareils(recipes);
-			const updatedUstensils = getAllUstensils(recipes);
+			const ingredients = getAllIngredients(recipes);
+			const appareils = getAllAppareils(recipes);
+			const ustensils = getAllUstensils(recipes);
 
 			updateRecipeCard(recipes, nbRecipes);
 
-			updateDataInDropdownMenu(ingreInDropdown, updatedIngredients, hiddenIngredientsList, recipes);
-			updateDataInDropdownMenu(apparInDropdown, updatedAppareils, hiddenAppareilsList, recipes);
-			updateDataInDropdownMenu(ustenInDropdown, updatedUstensils, hiddenUstensilsList, recipes);
+			updateDataInDropdownMenu(ingreInDropdown, ingredients, hiddenIngredientsList, recipes);
+			updateDataInDropdownMenu(apparInDropdown, appareils, hiddenAppareilsList, recipes);
+			updateDataInDropdownMenu(ustenInDropdown, ustensils, hiddenUstensilsList, recipes);
+
+			ingreInput.addEventListener("input", (e) => {
+				filteredInDropdownMenu(ingreInDropdown, e, ingredients, hiddenIngredientsList, recipes);
+			})
+			apparInput.addEventListener("input", (e) => {
+				filteredInDropdownMenu(apparInDropdown, e, appareils, hiddenAppareilsList, recipes);
+			})
+			ustenInput.addEventListener("input", (e) => {
+				filteredInDropdownMenu(ustenInDropdown, e, ustensils, hiddenUstensilsList, recipes);
+			})
 
 		} else {
 		
-
-		// const filteredDatas = Array.from(noeudParent.children).forEach(element => datasFromRecipes.filter(datas => datas.ingredients.find(data => data.ingredient.toLowerCase() === element)
-		// || datas.appliance.trim().toLowerCase().includes(element) 
-		// || datas.ustensils.find(data => data.toLowerCase() === element)));
-		console.log(itemsInNoeudParent);
-		// const filteredDatas = datasFromRecipes.filter(datas =>
-		// 	itemsInNoeudParent.forEach(element =>
-		// 		datas.appliance.trim().toLowerCase().includes(element.textContent.toLowerCase())
-		// 		|| datas.ingredients.some(data => data.ingredient.toLowerCase().includes(element.textContent.toLowerCase()))
-		// 		|| datas.ustensils.some(data => data.toLowerCase().includes(element.textContent.toLowerCase()))
-		// 	)
-		// );
 		 let filteredDatas = recipes;
-		 itemsInNoeudParent.forEach(element => {
+		 itemsInTagsSection.forEach(element => {
 			filteredDatas = filteredDatas.filter(data =>
 				data.appliance.trim().toLowerCase().includes(element.textContent.toLowerCase())
 				|| data.ingredients.some(data => data.ingredient.toLowerCase().includes(element.textContent.toLowerCase()))
@@ -316,16 +222,6 @@ function filteredWithTags(datasFromRecipes) {
 		 }
 		);
 
-		// const filteredDatas = itemsInNoeudParent.forEach(element => {
-		// 	datasFromRecipes.filter(datas =>
-		// 		datas.appliance.trim().toLowerCase().includes(element.textContent.toLowerCase())
-		// 		|| datas.ingredients.some(data => data.ingredient.toLowerCase().includes(element.textContent.toLowerCase()))
-		// 		|| datas.ustensils.some(data => data.toLowerCase().includes(element.textContent.toLowerCase()))
-		// 	)
-		// }
-		// );
-
-		console.log(filteredDatas);
 		const updatedIngredients = getAllIngredients(filteredDatas);
 		const updatedAppareils = getAllAppareils(filteredDatas);
 		const updatedUstensils = getAllUstensils(filteredDatas);
@@ -335,8 +231,16 @@ function filteredWithTags(datasFromRecipes) {
 		updateDataInDropdownMenu(ingreInDropdown, updatedIngredients, hiddenIngredientsList, filteredDatas);
 		updateDataInDropdownMenu(apparInDropdown, updatedAppareils, hiddenAppareilsList, filteredDatas);
 		updateDataInDropdownMenu(ustenInDropdown, updatedUstensils, hiddenUstensilsList, filteredDatas);
-	
-		
+
+		ingreInput.addEventListener("input", (e) => {
+			filteredInDropdownMenu(ingreInDropdown, e, updatedIngredients, hiddenIngredientsList, recipes);
+		})
+		apparInput.addEventListener("input", (e) => {
+			filteredInDropdownMenu(apparInDropdown, e, updatedAppareils, hiddenAppareilsList, recipes);
+		})
+		ustenInput.addEventListener("input", (e) => {
+			filteredInDropdownMenu(ustenInDropdown, e, updatedUstensils, hiddenUstensilsList, recipes);
+		})
 		}
 }
 
@@ -362,7 +266,7 @@ function displayDataInDropdownMenu(noeud, datas, selectList, datasFromRecipes) {
 		
 		li.addEventListener("click", e => {
 			if (e.target.innerText){
-				if (!selectList.children.length || !(isAlreadyInChild(selectList, e.target.innerText))) {
+				if (!selectList.children.length || !(isAlreadyInChild(selectList, e.target.innerText.trim().toLowerCase()))) {
 					createSearchContainer(e.target.innerText, selectList, datasFromRecipes);
 					createContainerInDropdown(e.target.innerText, selectList);
 					selectList.classList.remove('hidden');
@@ -390,7 +294,6 @@ function updateDataInDropdownMenu(noeud, datas, selectList, datasFromRecipes) {
 					createContainerInDropdown(e.target.innerText, selectList, datasFromRecipes);
 					selectList.classList.remove('hidden');
 					filteredWithTags(datasFromRecipes);
-					// console.log("data in update", datasFromRecipes)
 				}
 			}
 		})
@@ -400,8 +303,6 @@ function updateDataInDropdownMenu(noeud, datas, selectList, datasFromRecipes) {
 function filteredInDropdownMenu(noeud, e, datas, selectList, datasFromRecipes) {
 
 	const eventValue = e.target.value.trim().toLowerCase();
-
-	// console.log(e.target);
 
 	if (!eventValue || eventValue.length < 3) {
 		updateDataInDropdownMenu(noeud, datas, selectList, datasFromRecipes);
@@ -452,16 +353,16 @@ function updateRecipeCard(datas) {
 
 function filteredRecipeCard(datas, event, ingredients, appareils, ustensils, ingreInput, apparInput, ustenInput) {
 
+	const itemsInTagsSection = Array.from(tagsSection.children);
+
 	const eventValue = event.trim().toLowerCase();
 	
-	if (!eventValue || eventValue.length < 3) {
+	if ((!eventValue || eventValue.length < 3) && !itemsInTagsSection.length) {
 		updateRecipeCard(datas, nbRecipes);
 
 		updateDataInDropdownMenu(ingreInDropdown, ingredients, hiddenIngredientsList, datas);
 		updateDataInDropdownMenu(apparInDropdown, appareils, hiddenAppareilsList, datas);
 		updateDataInDropdownMenu(ustenInDropdown, ustensils, hiddenUstensilsList, datas);
-
-		console.log("datas", datas)
 
 		ingreInput.addEventListener("input", (e) => {
 			filteredInDropdownMenu(ingreInDropdown, e, ingredients, hiddenIngredientsList);
@@ -478,19 +379,8 @@ function filteredRecipeCard(datas, event, ingredients, appareils, ustensils, ing
 		|| element.description.toLowerCase().includes(eventValue) 
 		|| element.ingredients.forEach(data => {
 			data.ingredient.toLowerCase().includes(eventValue);
-			if(data.ingredient.toLowerCase().includes(eventValue)) {
-				console.log("true include");
-			}
 		})
 		);
-		// if (childrens) {
-			// filteredDatas = filteredDatas.filter(element => element.ingredients.find(data => data.ingredient.toLowerCase() === eventValue)
-			// || element.appliance.trim().toLowerCase().includes(eventValue) 
-			// || element.ustensils.find(data =>
-			// 	data.toLowerCase() === eventValue
-			// ));
-		// }
-		// filtrer en fonction des childrens et plus de l'event =/= filterwithtags
 		
 		const updatedIngredients = getAllIngredients(filteredDatas);
 		const updatedAppareils = getAllAppareils(filteredDatas);
@@ -515,11 +405,10 @@ function filteredRecipeCard(datas, event, ingredients, appareils, ustensils, ing
 }
 
 function displayPage() {
-	const ingreInput = document.getElementById("ingredient_input");
-	const apparInput = document.getElementById("appareils_input");
-	const ustenInput = document.getElementById("ustensiles_input");
 
 	const mainInput = document.getElementById("main_input");
+
+	const mainInputButton = document.getElementById("main_input_button");
 
 	const ingredients = getAllIngredients(recipes);
 	const appareils = getAllAppareils(recipes);
@@ -543,6 +432,16 @@ function displayPage() {
 
 	mainInput.addEventListener("input", e => {
 		filteredRecipeCard(recipes, e.target.value, ingredients, appareils, ustensils, ingreInput, apparInput, ustenInput);
+	})
+
+	mainInputButton.addEventListener("click", () => {
+		const inputValue = mainInput.value.trim().toLowerCase();
+
+		if (inputValue.length >= 3) {
+			createSearchContainerFromMainInput(inputValue);
+	
+			filteredWithTags(recipes);
+		}
 	})
 
 	openDropdownMenu();
